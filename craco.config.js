ac7dd187827583f92324c46e197a026a23e9853e
Ang Lee <ang.lee@gmail.com>
2020-08-23 14:01:37 -0400

    Add Ant Design and styled

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#FF2F92' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.less";
+import { Button } from "antd";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.6em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Hello world!</Title>
+      <Button type="primary" className="my-button" onClick={() => null}>
+        Button
+      </Button>
     </div>
   );
 }
