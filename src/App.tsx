d0de2055b47c7f0e2470128e2a9fee27fc6684d2
Ang Lee <ang.lee@gmail.com>
2020-09-02 16:40:13 -0400

    material UI 4.11.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.less";
 import { Button } from "antd";
 import styled from "styled-components";
+import MButton from '@material-ui/core/Button';
 
 const Title = styled.h1`
   font-size: 1.6em;
@@ -16,6 +17,14 @@ function App() {
       <Button type="primary" className="my-button" onClick={() => null}>
         Button
       </Button>
+
+      <MButton variant="contained">Default</MButton>
+      <MButton variant="contained" color="primary">
+        Primary
+      </MButton>
+      <MButton variant="contained" color="secondary">
+        Secondary
+      </MButton>
     </div>
   );
 }
